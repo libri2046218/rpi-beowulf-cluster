a3f9787113dbf99f91f33946330038208fcf9fba
Pietro Francesco Libri <pietrofrancesco.libri@gmail.com>
2025-08-06 15:17:53 +0200

    Added algo folder to contain all parallel algorithms developed - Added mpi_hello.c test file

---
+++ algo/mpi_hello.c
@@ -0,0 +1,28 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    // Inizializza l'ambiente MPI
+    MPI_Init(&argc, &argv);
+
+    // Ottieni il numero totale di processi
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Ottieni il rank del processo
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    // Ottieni il nome del processore
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
+
+    // Stampa messaggio da ogni processo
+    printf("Hello from processor %s, rank %d out of %d processors\n",
+           processor_name, world_rank, world_size);
+
+    // Finalizza l'ambiente MPI
+    MPI_Finalize();
+    return 0;
+}
