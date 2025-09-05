a9f55d1f59482898193d608cd5b3fd87eb922532
Pietro Francesco Libri <pietrofrancesco.libri@gmail.com>
2025-09-05 11:15:32 +0200

    Added comments

--- algo/sorting/merge_sort_distributed.c
+++ algo/sorting/merge_sort_distributed.c
@@ -20,7 +20,7 @@ void read_vector(
             // Genera un array
             array_random(arr, n);
 
-            array_print(arr, n);
+            //array_print(arr, n);
 
             MPI_Scatterv(arr, local_n, displs, MPI_INT, local_arr, local_n[my_rank], MPI_INT, 0, comm);
             free(arr);
@@ -30,15 +30,10 @@ void read_vector(
 
 }
 
-void distributed_merge(){
-
-}
-
-
-
 int main(int argc, char* argv[]) {
 
-    int n = atoi(argv[1]); // Dimensione dell'array
+    // La dimensione dell'array viene letta in input
+    int n = atoi(argv[1]); 
 
     int comm_sz;
     int my_rank;
@@ -47,6 +42,7 @@ int main(int argc, char* argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
+    // Distribuzione equa del carico
     int local_n[comm_sz];
     for (int i = 0; i < comm_sz; i++) {
         local_n[i] = n / comm_sz;
@@ -58,26 +54,32 @@ int main(int argc, char* argv[]) {
         local_n[i] += 1;
     }
 
+    // Inizializzazione del vettore che contiene l'informazione di displacement
     int displs[comm_sz];
     displs[0] = 0;
     for (int i = 1; i < comm_sz; i++){
         displs[i] = displs[i-1] + local_n[i-1];
     }
 
+    // Allocazione di memoria dinamica per il vettore locale da ordinare
     int* local_arr = (int*)malloc(local_n[my_rank] * sizeof(int));
 
+    // Il process con rank 0 genera un vettore casuale di lunghezza n e lo invia agli altri processi
+    // Gli altri processi ricevono dal processo 0 una parte del vettore originale e lo salvano in locale
     read_vector(local_arr, local_n, displs, n, my_rank, MPI_COMM_WORLD);
 
     
-    printf("Process %d: ", my_rank);
-    array_print(local_arr, local_n[my_rank]);
+    //printf("Process %d: ", my_rank);
+    //array_print(local_arr, local_n[my_rank]);
 
+    // Il vettore locale viene ordinato 
     merge_sort(local_arr, 0, local_n[my_rank] - 1);
 
-    printf("Process %d: ", my_rank);
-    array_print(local_arr, local_n[my_rank]);
+    //printf("Process %d: ", my_rank);
+    //array_print(local_arr, local_n[my_rank]);
 
 
+    // Inizializzazione di variabili ausiliarie per il cambio di comunicatore
     int new_comm_sz = comm_sz;
     int new_rank = my_rank;
     int color;
@@ -85,18 +87,20 @@ int main(int argc, char* argv[]) {
     
     while(new_comm_sz > 1) {
 
-        MPI_Barrier(new_comm);
+        //MPI_Barrier(new_comm);
 
-        MPI_Comm_rank(new_comm, &new_rank);
-        MPI_Comm_size(new_comm, &new_comm_sz);
-
+        // I processi con rank dispari inviano il loro vettore locale
         if (new_rank % 2 == 1) {
 
             MPI_Send(local_arr, local_n[new_rank], MPI_INT, new_rank - 1, 0, new_comm);
 
+            // Il processo non verrà incluso nel nuovo communicator
             color = MPI_UNDEFINED;
 
-        } else if (new_rank % 2 == 0 && new_rank + 1 != new_comm_sz) {
+        } 
+        // I processi con rank pari allocano spazio sia per il loro vettore che per quello che ricevono
+        // e chiamano la procedura di merge per ordinare il nuovo vettore, sapendo che i due vettori sono già ordinati
+        else if (new_rank % 2 == 0 && new_rank + 1 != new_comm_sz) {
 
             int* new_local_arr = (int*)malloc((local_n[new_rank] + local_n[new_rank + 1]) * sizeof(int));
 
@@ -106,28 +110,45 @@ int main(int argc, char* argv[]) {
 
             merge(new_local_arr, 0, local_n[new_rank + 1] - 1, local_n[new_rank] + local_n[new_rank + 1] - 1);
 
-            array_print(new_local_arr, local_n[new_rank] + local_n[new_rank + 1]);
+            //array_print(new_local_arr, local_n[new_rank] + local_n[new_rank + 1]);
 
+            //Deallocazione della memoria non più utilizzata
             free(local_arr);
             local_arr = new_local_arr;
 
+            // Il processo farà parte del nuovo communicator
             color = 1;
 
         } else {
+            // Il processo farà parte del nuovo communicator
             color = 1;
         }
 
+        // Il vettore che mantiene in memoria l'informazione del carico per ogni nodo viene aggiornato
         array_reduce(local_n, new_comm_sz);
         
+        // Viene creato un nuovo comunicatore
         MPI_Comm old_comm = new_comm;
-
         MPI_Comm_split(old_comm, color, new_rank, &new_comm);
 
+        // Viene liberato lo spazio del vecchio comunicatore
         if (old_comm != MPI_COMM_WORLD) MPI_Comm_free(&old_comm);
+
+        // I processi non presenti nel nuovo comunicatore escono dal ciclo
         if (color == MPI_UNDEFINED) break;
 
-        
+        // Assegnazione di nuovi rank
+        MPI_Comm_rank(new_comm, &new_rank);
+        MPI_Comm_size(new_comm, &new_comm_sz);
     }
+
+    // L'ultimo processo verifica la correttezza
+    if (new_rank == 0) {
+        int check = array_check_sorting(local_arr, n);
+        printf("Check: %d\n", check);
+    }
+
+    free(local_arr);
 
     MPI_Finalize();
 
