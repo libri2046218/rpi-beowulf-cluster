94e39dec3d9309b70d1b39169ba3cac2dfe943ad
Pietro Francesco Libri <pietrofrancesco.libri@gmail.com>
2025-09-08 16:00:57 +0200

    Folder renamed

---
+++ algo/matrices/matrix.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "matrix.h"
+
+Matrix* matrix_alloc(int n) {
+
+	Matrix* ret = (Matrix*)malloc(sizeof(Matrix));
+	
+	int* M = (int*)malloc(n * n * sizeof(int));
+	
+	ret->M = M;
+	ret->n = n;
+	
+	return ret;
+}
+
+void matrix_free(Matrix* m) {
+	int n = m->n;
+
+	free(m->M);
+	
+	free(m);
+}
+
+void matrix_get(Matrix* mat, int row, int col, int* out){
+
+	int offset = row * mat->n + col;
+
+	*out = (mat->M)[offset];
+
+}
+
+void matrix_set(Matrix* mat, int row, int  col, int in) {
+	int offset = row * mat->n + col;
+	(mat->M)[offset] = in;
+}
+
+void matrix_transpose(Matrix* mat) {
+	int n = mat->n;
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < i; j++) {
+			int aux_1;
+			int aux_2;
+			matrix_get(mat, i, j, &aux_1);
+			matrix_get(mat, j, i, &aux_2);
+			matrix_set(mat, i, j, aux_2);
+			matrix_set(mat, j, i, aux_1);
+		}
+	}
+}
+
+void matrix_print(Matrix* m) {
+	int n = m->n;
+	
+	printf("(\n");
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++) {
+			int val;
+			matrix_get(m, i, j, &val);
+			printf("%d, ", val);
+		}
+		printf("\n");
+	}
+	printf(")\n");
+}
+
+void matrix_random(Matrix* out) {
+	
+	int n = out->n;
+
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++) {
+			matrix_set(out, i, j, rand() % n);
+		}
+	}
+}
+
+void matrix_empty(Matrix* out) {
+
+	int n = out->n;
+	
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++) {
+			matrix_set(out, i, j, 0);
+		}
+	}
+}
+
---
+++ algo/matrices/matrix.h
@@ -0,0 +1,22 @@
+typedef struct {
+
+	int* M;
+	int n;
+
+} Matrix;
+
+Matrix* matrix_alloc(int n);
+
+void matrix_free(Matrix* m);
+
+void matrix_print(Matrix* m);
+
+void matrix_get(Matrix* mat, int row, int col, int* out);
+
+void matrix_set(Matrix* mat, int row, int col, int in);
+
+void matrix_transpose(Matrix* mat);
+
+void matrix_random(Matrix* out);
+
+void matrix_empty(Matrix* out);
